6d41b4061f402a6728d66c54c90ac1c0840a53f1
Komminenianju <komminenianjali8@gmail.com>
2025-07-08 14:18:36 +0530

    Add files via upload

---
+++ india.py
@@ -0,0 +1,18 @@
+'''
+list1=[1,2,3,5,7,6,4]
+i=0
+j=len(list1)-1
+while(i!=j):
+    temp=list1[i];
+    list1[i]=list1[j];
+    list1[j]=temp;
+    i+=1
+    j-=1
+print(list1)
+'''
+n=int(input("enter your number")
+      for i in range(i,n+1):
+      if(n>i):
+
+      
+      
