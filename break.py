a1304fefaf1eaf136d067deaddea8502ed910515
Komminenianju <komminenianjali8@gmail.com>
2025-07-26 10:22:19 +0530

    Add files via upload

--- break.py
+++ break.py
@@ -1,19 +1,5 @@
-'''
-s=input()
-s2=s.lower()
-a=s2.count('a')
-e=s2.count('e')
-i=s2.count('i')
-o=s2.count('o')
-u=s2.count('u')
-print(f"number of vowels:{a+e+i+o+u}")
-'''
-year=int(input())
-leap=false
-if year%100==0 and year%400==0:
-    leap=true
-elif year%4==0:
-    leap=true
-else:
-    leap=false
-print(leap)
+l=[10,20,30,40,50]
+sum=0
+for i in l:
+    sum=sum+i
+print(sum)
